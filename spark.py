111cf6d6bec12c3128a7d7541a1c44c45f5793ac
Gabriel <gaby.tzu@windowslive.com>
2021-09-21 12:23:17 +0300

    Update spark.py

--- spark.py
+++ spark.py
@@ -3,46 +3,76 @@ os.environ["JAVA_HOME"] = "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/
 os.environ["SPARK_HOME"] = "/Users/gpintoiu/Downloads/spark-3.1.2-bin-hadoop3.2"
 
 import findspark
-from datetime import datetime
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
 from pyspark.sql.types import StringType
-from pyspark.sql.functions import row_number, lit, col, array, round, size, when, concat, concat_ws, array_except, element_at
+from pyspark.sql.functions import arrays_zip, row_number, lit, col, concat, element_at, explode, to_timestamp, round
 
-presentationMode = True
+presentationMode = False
+
 
 # initialize the PySpark session
 findspark.init()
 spark = SparkSession.builder.master("local[*]").appName("spark_TC").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
+print("______________________________")
 
-# create the data sets
-stations = [(0, "BAutogara"),(1, "BVAutogara"),(2, "SBAutogara"), (3, "CJAutogara"),(4, "MMAutogara"),
-            (5, "ISAutogara"), (6, "CTAutogara"),(7, "TMAutogara"),(8, "BCAutogara"), (9, "MSAutogara")]
-trips = [("B", "SB", [0,2], [datetime(2021, 3, 1, 6, 0, 00), datetime(2021, 3, 1, 9, 10, 00)]), \
-        ("B", "MM", [0,2,4], [datetime(2021, 3, 1, 10, 10, 00), datetime(2021, 3, 1, 12, 20, 10), datetime(2021, 3, 1, 14, 10, 10)]), \
-        ("BV", "IS", [1,8,3,5], [datetime(2021, 3, 1, 8, 10, 00), datetime(2021, 3, 1, 12, 20, 10), datetime(2021, 3, 1, 15, 10, 10), datetime(2021, 3, 1, 15, 45, 00)]), \
-        ("TM", "CT", [7,2,9,4,6], [datetime(2021, 4, 1, 10, 45, 00), datetime(2021, 4, 1, 12, 20, 10), datetime(2021, 4, 1, 19, 30, 10), datetime(2021, 4, 1, 21, 50, 10), datetime(2021, 4, 1, 22, 00, 30)]), \
-        ("CJ", "BC", [3,9,5,6,7,8], [datetime(2021, 5, 1, 7, 10, 00), datetime(2021, 5, 1, 12, 20, 00), datetime(2021, 5, 1, 13, 25, 10), datetime(2021, 5, 1, 14, 35, 10), datetime(2021, 5, 1, 15, 45, 10), datetime(2021, 5, 1, 21, 20, 10)])]
+# create the data sets and pass them to dataframes
+stations = ({
+    "internal_bus_station_id": [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    ],
+    "public_bus_station": [
+        "BAutogara", "BVAutogara", "SBAutogara", "CJAutogara", "MMAutogara","ISAutogara", "CTAutogara", "TMAutogara", "BCAutogara", "MSAutogara"
+    ]
+})
+trips = ({
+    "origin": [
+        "B","B","BV","TM","CJ"
+    ],
+    "destination": [
+        "SB","MM","IS","CT","BC"
+    ],
+    "internal_bus_station_ids": [
+        [0,2],[0,2,4],[1,8,3,5],[7,2,9,4,6],[3,9,5,6,7,8]
+    ],
+    "TRIPTIMES": [
+    ["2021-03-01 06:00:00", "2021-03-01 09:10:00"],
+    ["2021-03-01 10:10:00", "2021-03-01 12:20:10", "2021-03-01 14:10:10"],
+    ["2021-04-01 08:10:00", "2021-04-01 12:20:10", "2021-04-01 15:10:00", "2021-04-01 15:45:00"],
+    ["2021-05-01 10:45:00", "2021-05-01 12:20:10", "2021-05-01 18:30:00", "2021-05-01 20:45:00", "2021-05-01 22:00:00"],
+    ["2021-05-01 07:10:00", "2021-05-01 10:20:00", "2021-05-01 12:30:00", "2021-05-01 13:25:00", "2021-05-01 14:35:00", "2021-05-01 15:45:00", "2021-05-01 21:20:25"]
+    ]
+})
 
-stationsColumns = ["internal_bus_station_id", "public_bus_station"]
-tripsColumns = ["origin", "destination", "internal_bus_stations_ids", "triptimes"]
-stationsDF = spark.createDataFrame(data=stations, schema=stationsColumns)
-tripsDF = spark.createDataFrame(data=trips, schema=tripsColumns)
+stationsDF = spark.read.json(spark.sparkContext.parallelize([stations]), multiLine=True)
+tripsDF = spark.read.json(spark.sparkContext.parallelize([trips]), multiLine=True)
 
-# generate the row number columns for both dataframes
+if presentationMode:
+    print("\n\nAdd the files into the DataFrames:")
+    stationsDF.show()
+    tripsDF.show()
+
+tripsDF = tripsDF.withColumn("new", arrays_zip("origin", "destination", "internal_bus_station_ids", "TRIPTIMES")).withColumn("new", explode("new"))\
+        .select(col("new.origin"), col("new.destination"), col("new.internal_bus_station_ids").alias("internal_bus_stations_ids"), col("new.TRIPTIMES").alias("triptimes"))
+stationsDF = stationsDF.withColumn("new", arrays_zip("internal_bus_station_id", "public_bus_station")).withColumn("new", explode("new"))\
+            .select(col("new.internal_bus_station_id"), col("new.public_bus_station"))
+    
+# generate the rows count column
 w = Window().orderBy(lit('A'))
 stationsDF = stationsDF.withColumn("row_num", row_number().over(w)).select("row_num", "internal_bus_station_id", "public_bus_station")
 tripsDF = tripsDF.withColumn("row_num", row_number().over(w)).select("row_num", "origin", "destination", "internal_bus_stations_ids", "triptimes")
 
-print("_____ \n\n Stations data set:")
+if presentationMode:
+    print("'Explode' the arrays (add each array element in its own row), and add the rows numbers:")
+
+print("\n\n Stations data set:")
 stationsDF.show()
 print("Trips data set:")
 tripsDF.show(truncate=False)
 
-
 if presentationMode:
-    print("Calculate the trip duration for each bus stop:")
+    print("Calculate the trip duration for each bus stop, converting each string array element to timestamp on the fly:")
     trips = tripsDF.select("triptimes").collect()
     stopNumber = 1
     prevStop = 0
@@ -51,7 +81,7 @@ if presentationMode:
     for _ in trips:
         stopName = f"stop_{stopNumber}"
         columnsList.append(f"duration_in_h_{str(stopName)}")
-        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", lit(tripsDF.triptimes[stopNumber] - tripsDF.triptimes[prevStop]))
+        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", lit(to_timestamp(tripsDF.triptimes[stopNumber]) - to_timestamp(tripsDF.triptimes[prevStop])))
         tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", col(f"duration_in_h_{str(stopName)}").cast(StringType()))
         stopNumber +=1
         prevStop += 1
@@ -60,13 +90,15 @@ if presentationMode:
     print("Remove null values:")
     tripsDF = tripsDF.na.fill("")
     tripsDF.show(truncate=False)
-
+    tripsDF = tripsDF.withColumn("duration_in_h_total", lit(to_timestamp(element_at(tripsDF.triptimes, -1)) - to_timestamp(tripsDF.triptimes[0])))
+    tripsDF = tripsDF.withColumn("duration_in_h_total", col("duration_in_h_total").cast(StringType()))
+    
 # calculate the duration from triptimes[0] to triptimes[len(triptimes) -1]
-tripsDF = tripsDF.withColumn("duration", lit(round(element_at(tripsDF.triptimes, -1).cast("int") - tripsDF.triptimes[0].cast("int"))/60))
-tripsDF = tripsDF.withColumn("duration", round(tripsDF["duration"],2))
+tripsDF = tripsDF.withColumn("duration", lit(to_timestamp(element_at(tripsDF.triptimes, -1)).cast("long") - to_timestamp(tripsDF.triptimes[0]).cast("long")))
+tripsDF = tripsDF.withColumn("duration", round(col("duration")/60, 2))
+tripsDF = tripsDF.withColumn("duration", col("duration").cast(StringType()))
 tripsDF = tripsDF.withColumn("duration", concat(col("duration"), lit(" min")))
-tripsDF = tripsDF.withColumn("duration_in_h_total", lit(element_at(tripsDF.triptimes, -1) - tripsDF.triptimes[0]))
-tripsDF = tripsDF.withColumn("duration_in_h_total", col("duration_in_h_total").cast(StringType()))
+
 if presentationMode:
     print("Calculate the total trips duration:")
     tripsDF.show(truncate=False)
@@ -93,7 +125,7 @@ if presentationMode:
 tripsListId = []
 tripsListName = []
 
-# grab the internal_bus_stations_ids arrays and save the arrays in the 'tripsListId' list: [id, id, id], [id, id, id]
+# get the internal_bus_stations_ids arrays and save the arrays in the 'tripsListId' list: [id, id, id], [id, id, id]
 # loop through each accessed array element and use it as dict key to retrieve the public bus station: {id: bus station}
 # the retrieved dict results from each array are saved in the 'temp' list: [name, name, name]
 # during each array iteration, the temp list is saved in the 'tripsListName' list of lists: [name, name, name], [name, name, name]
