fdcb280bec181a88df110686967649db921e91b6
Gabriel <gaby.tzu@windowslive.com>
2021-09-21 18:25:52 +0300

    Update spark.py

--- spark.py
+++ spark.py
@@ -8,14 +8,14 @@ from pyspark.sql.window import Window
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import arrays_zip, row_number, lit, col, concat, element_at, explode, to_timestamp, round
 
-presentationMode = True
+presentationMode = False
 
 
 # initialize the PySpark session
 findspark.init()
 spark = SparkSession.builder.master("local[*]").appName("spark_TC").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
-print("______________________________")
+print("______________________________\n")
 
 # read the JSON data sets and pass them to dataframes
 stationsPath = "/Users/gpintoiu/Desktop/stations.json"
@@ -48,48 +48,16 @@ if presentationMode:
 
 if presentationMode:
     print("Add the rows numbers.")
+
 # generate the rows count column
 w = Window().orderBy(lit('A'))
 stationsDF = stationsDF.withColumn("row_num", row_number().over(w)).select("row_num", "internal_bus_station_id", "public_bus_station")
 tripsDF = tripsDF.withColumn("row_num", row_number().over(w)).select("row_num", "origin", "destination", "internal_bus_stations_ids", "triptimes")
 
-
-print("\n\n Stations data set:")
+print("\nStations data set:")
 stationsDF.show()
 print("Trips data set:")
 tripsDF.show(truncate=False)
-
-if presentationMode:
-    print("Calculate the trip duration for each bus stop, converting each string array element to timestamp on the fly:")
-    trips = tripsDF.select("triptimes").collect()
-    stopNumber = 1
-    prevStop = 0
-    columnsList = []
-
-    for _ in trips:
-        stopName = f"stop_{stopNumber}"
-        columnsList.append(f"duration_in_h_{str(stopName)}")
-        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", lit(to_timestamp(tripsDF.triptimes[stopNumber]) - to_timestamp(tripsDF.triptimes[prevStop])))
-        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", col(f"duration_in_h_{str(stopName)}").cast(StringType()))
-        stopNumber +=1
-        prevStop += 1
-        tripsDF.show(truncate=False)
-
-    print("Remove null values:")
-    tripsDF = tripsDF.na.fill("")
-    tripsDF.show(truncate=False)
-    tripsDF = tripsDF.withColumn("duration_in_h_total", lit(to_timestamp(element_at(tripsDF.triptimes, -1)) - to_timestamp(tripsDF.triptimes[0])))
-    tripsDF = tripsDF.withColumn("duration_in_h_total", col("duration_in_h_total").cast(StringType()))
-    
-# calculate the duration from triptimes[0] to triptimes[len(triptimes) -1]
-tripsDF = tripsDF.withColumn("duration", lit(to_timestamp(element_at(tripsDF.triptimes, -1)).cast("long") - to_timestamp(tripsDF.triptimes[0]).cast("long")))
-tripsDF = tripsDF.withColumn("duration", round(col("duration")/60, 2))
-tripsDF = tripsDF.withColumn("duration", col("duration").cast(StringType()))
-tripsDF = tripsDF.withColumn("duration", concat(col("duration"), lit(" min")))
-
-if presentationMode:
-    print("Calculate the total trips duration:")
-    tripsDF.show(truncate=False)
 
 # get the internal stationsDF's columns and save them into tow distinct lists
 internalBus = stationsDF.select("internal_bus_station_id").collect()
@@ -110,13 +78,14 @@ if presentationMode:
     print(f"Retrieve the internal ids: {internalBusList} \nRetrieve the public stations names: {publicBusList}")
     print(f"Save the retrieved values inside a dictionary {stationsDict}\n")
 
-tripsListId = []
-tripsListName = []
-
 # get the internal_bus_stations_ids arrays and save the arrays in the 'tripsListId' list: [id, id, id], [id, id, id]
 # loop through each accessed array element and use it as dict key to retrieve the public bus station: {id: bus station}
 # the retrieved dict results from each array are saved in the 'temp' list: [name, name, name]
 # during each array iteration, the temp list is saved in the 'tripsListName' list of lists: [name, name, name], [name, name, name]
+
+tripsListId = []
+tripsListName = []
+maxBusStops = 0
 
 tripsArr = tripsDF.select("internal_bus_stations_ids").collect()
 for i in tripsArr:
@@ -124,6 +93,8 @@ for i in tripsArr:
     for j in i:
         # array
         temp = []
+        if maxBusStops < len(j):
+            maxBusStops = len(j)
         if presentationMode:
             print(f"Get the trips ids {j}")
         tripsListId.append(j)
@@ -133,6 +104,36 @@ for i in tripsArr:
         if presentationMode:
             print(f"Get the trips bus stops names {temp}")
         tripsListName.append(temp)
+
+if presentationMode:
+    print("\nCalculate the trip duration for each bus stop, converting each string array element to timestamp on the fly:")
+    trips = tripsDF.select("triptimes").collect()
+    stopNumber = 1
+    columnsList = []
+
+    for i in range(maxBusStops -1):
+        stopName = f"stop_{stopNumber}"
+        columnsList.append(f"duration_in_h_{str(stopName)}")
+        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", lit(to_timestamp(tripsDF.triptimes[stopNumber]) - to_timestamp(tripsDF.triptimes[i])))
+        tripsDF = tripsDF.withColumn(f"duration_in_h_{str(stopName)}", col(f"duration_in_h_{str(stopName)}").cast(StringType()))
+        stopNumber +=1
+        tripsDF.show()
+
+    print("Remove null values:")
+    tripsDF = tripsDF.na.fill("")
+    tripsDF.show()
+    tripsDF = tripsDF.withColumn("duration_in_h_total", lit(to_timestamp(element_at(tripsDF.triptimes, -1)) - to_timestamp(tripsDF.triptimes[0])))
+    tripsDF = tripsDF.withColumn("duration_in_h_total", col("duration_in_h_total").cast(StringType()))
+    
+# calculate the duration from triptimes[0] to triptimes[len(triptimes) -1]
+tripsDF = tripsDF.withColumn("duration", lit(to_timestamp(element_at(tripsDF.triptimes, -1)).cast("long") - to_timestamp(tripsDF.triptimes[0]).cast("long")))
+tripsDF = tripsDF.withColumn("duration", round(col("duration")/60, 2))
+tripsDF = tripsDF.withColumn("duration", col("duration").cast(StringType()))
+tripsDF = tripsDF.withColumn("duration", concat(col("duration"), lit(" min")))
+
+if presentationMode:
+    print("Calculate the total trips duration:")
+    tripsDF.show()
 
 # create the 'trips_with_ids' table out of the 'tripsListId' list and 'the tripsListName' list of lists
 trips_with_ids = spark.createDataFrame(zip(tripsListId, tripsListName), schema=['internal', 'public'])
